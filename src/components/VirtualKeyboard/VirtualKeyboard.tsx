a8b55ba388fbcd354d7fbc134d2f2c66b90af4c5
bankai671 <pavelvlas671@gmail.com>
2023-07-28 00:18:55 +0200

    add: init VirtualKeyboard component and css module for it

---
+++ src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './VirtualKeyboard.module.css';
+
+export const VirtualKeyboard = () => {
+  return <div className={styles.keyboard}>VirtualKeyboard</div>;
+};
