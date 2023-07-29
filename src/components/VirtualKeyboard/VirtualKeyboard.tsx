7f33deb8716e7bbf4f4db14ac1a61d3cb89ae427
bankai671 <pavelvlas671@gmail.com>
2023-07-30 00:09:05 +0200

    styles: styled VirtualKeyboard component

--- src/components/VirtualKeyboard/VirtualKeyboard.tsx
+++ src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import styles from './VirtualKeyboard.module.css';
+import styles from './VirtualKeyboard.module.less';
 
 export const VirtualKeyboard = () => {
-  return <div className={styles.keyboard}>VirtualKeyboard</div>;
+  const FIRST_ROW = [...'QWERTYUIOP'];
+  const SECOND_ROW = [...'ASDFGHJKL'];
+  const THIRD_ROW = ['Enter', ...'ZXCVBNM', 'Delete'];
+
+  return (
+    <div className={styles.keyboard}>
+      <div className={styles.row}>
+        {FIRST_ROW.map((key) => (
+          <button className={styles.btn} key={Math.random() * 100}>{key}</button>
+        ))}
+      </div>
+      <div className={styles.row}>
+        {SECOND_ROW.map((key) => (
+          <button className={styles.btn} key={Math.random() * 100}>{key}</button>
+        ))}
+      </div>
+      <div className={styles.row}>
+        {THIRD_ROW.map((key) => (
+          <button className={styles.btn} key={Math.random() * 100}>{key}</button>
+        ))}
+      </div>
+    </div>
+  );
 };
