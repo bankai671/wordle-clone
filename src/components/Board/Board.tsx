9b069585ca53e78b522cf1b1737cbd87e7b090f7
bankai671 <pavelvlas671@gmail.com>
2023-07-29 18:06:35 +0200

    add: init board component and styled a little bit

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import styles from './Board.module.css';
 
+const BOARD_SCELETON = Array.from({ length: 6 }, () => Array(5).fill(0));
+
 export const Board = () => {
-  return <div className={styles.board}>Board</div>;
+  const [board, setBoard] = useState(BOARD_SCELETON);
+
+  useEffect(() => {
+    console.log(board);
+  }, []);
+
+  return (
+    <div className={styles.board}>
+      {board.map((row, y) => (
+        <div className={styles.row}>
+          {row.map((square, x) => (
+            <div className={styles.square}>
+              {y} - {x}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
