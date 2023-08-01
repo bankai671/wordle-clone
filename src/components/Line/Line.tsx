08f3ce64de9fb731ef8bc4040764bad1ecc51a71
bankai671 <pavelvlas671@gmail.com>
2023-08-01 21:49:05 +0200

    update: validate tiles

--- src/components/Line/Line.tsx
+++ src/components/Line/Line.tsx
@@ -6,14 +6,32 @@ const WORD_LENGTH = 5;
 
 interface LineProps {
   guess: string;
+  isFinal: boolean;
+  solution: string;
 }
 
-export const Line: FunctionComponent<LineProps> = ({ guess }) => {
+export const Line: FunctionComponent<LineProps> = ({ guess, isFinal, solution }) => {
   const tiles = [];
 
   for (let i = 0; i < WORD_LENGTH; i++) {
     const char = guess[i];
-    tiles.push(<div key={i} className={styles.tile}>{char}</div>);
+    const classNames = [styles.tile];
+
+    if (isFinal) {
+      if (char.toUpperCase() === solution[i].toUpperCase()) {
+        classNames.push(styles.correct);
+      } else if (solution.includes(char || char.toUpperCase())) {
+        classNames.push(styles.close);
+      } else {
+        classNames.push(styles.incorrect);
+      }
+    }
+
+    tiles.push(
+      <div key={i} className={classNames.join(' ')}>
+        {char}
+      </div>
+    );
   }
 
   return <div className={styles.line}>{tiles}</div>;
