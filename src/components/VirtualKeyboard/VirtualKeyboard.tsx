395c8e9936aa854eeab8dcf982540f12890050ef
bankai671 <pavelvlas671@gmail.com>
2023-08-07 20:13:34 +0200

    update: add click virtual keyboard handler

--- src/components/VirtualKeyboard/VirtualKeyboard.tsx
+++ src/components/VirtualKeyboard/VirtualKeyboard.tsx
@@ -5,25 +5,46 @@ export const VirtualKeyboard = () => {
   const SECOND_ROW = [...'ASDFGHJKL'];
   const THIRD_ROW = ['Enter', ...'ZXCVBNM', 'Delete'];
 
+  function handleKeyClick(key: string) {
+    dispatchEvent(
+      new KeyboardEvent('keydown', {
+        key: key === 'Delete' ? 'Backspace' : key,
+        view: window,
+      })
+    );
+  }
+
   return (
     <div className={styles.keyboard}>
       <div className={styles.row}>
         {FIRST_ROW.map((key) => (
-          <button className={styles.btn} key={key}>
+          <button
+            className={styles.btn}
+            key={key}
+            onClick={() => handleKeyClick(key)}
+          >
             {key}
           </button>
         ))}
       </div>
       <div className={styles.row}>
         {SECOND_ROW.map((key) => (
-          <button className={styles.btn} key={key}>
+          <button
+            className={styles.btn}
+            key={key}
+            onClick={() => handleKeyClick(key)}
+          >
             {key}
           </button>
         ))}
       </div>
       <div className={styles.row}>
         {THIRD_ROW.map((key) => (
-          <button className={styles.btn} key={key}>
+          <button
+            className={styles.btn}
+            key={key}
+            onClick={() => handleKeyClick(key)}
+          >
             {key}
           </button>
         ))}
